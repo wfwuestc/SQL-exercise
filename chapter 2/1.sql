077f70e2a49d27f8d24b6e6abf3004cea4ff991f
Wen <wfwuestc@163.com>
2019-12-02 16:07:53 +0800

    chapter 2 complete

---
+++ chapter 2/1.sql
@@ -0,0 +1,3 @@
+﻿SELECT product_name, regist_date
+  FROM Product
+ WHERE regist_date > '2009-04-28';
---
+++ chapter 2/3.sql
@@ -0,0 +1,9 @@
+﻿-- 1
+SELECT product_name, sale_price, purchase_price
+  FROM Product
+ WHERE NOT sale_price - purchase_price < 500;
+
+--2
+ SELECT product_name, sale_price, purchase_price
+  FROM Product
+ WHERE sale_price - 500 >= 500;
